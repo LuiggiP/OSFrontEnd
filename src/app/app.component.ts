72b758bad02e8e74f8de45fdd9d39413c5f0b579
LuiggiP <u202218996@upc.edu.pe>
2024-05-01 16:37:38 -0500

    feat(app-register): finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {DeviceInformationComponent} from "./components/device-information/device-information.component";
 import {LocationSharingComponent} from "./components/location-sharing/location-sharing.component";
 import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DeviceInformationComponent, LocationSharingComponent, LoginComponent],
+  imports: [RouterOutlet, DeviceInformationComponent, LocationSharingComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatCard, MatCardContent} from "@angular/material/card";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconButton,
+    MatIcon,
+    MatCard,
+    MatCardContent
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
