9fa51c9b84acea03b2fad8e941e0328e96940412
LuiggiP <u202218996@upc.edu.pe>
2024-05-01 16:16:41 -0500

    feat(app-login): finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {DeviceInformationComponent} from "./components/device-information/device-information.component";
 import {LocationSharingComponent} from "./components/location-sharing/location-sharing.component";
+import {LoginComponent} from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DeviceInformationComponent, LocationSharingComponent],
+  imports: [RouterOutlet, DeviceInformationComponent, LocationSharingComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
