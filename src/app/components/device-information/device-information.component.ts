6c8d1119c7e70176cb19410dd4a5f0fd5bc21987
LuiggiP <u202218996@upc.edu.pe>
2024-05-01 15:55:36 -0500

    feat(device-information): finished

--- src/app/components/device-information/device-information.component.ts
+++ src/app/components/device-information/device-information.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-device-information',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatCard,
+    MatCardContent,
+    MatButton
+  ],
   templateUrl: './device-information.component.html',
   styleUrl: './device-information.component.css'
 })
