d47ee1019bcec4cc2f6804462f79b541772fc0ca
LuiggiP <u202218996@upc.edu.pe>
2024-05-01 16:06:14 -0500

    feat(location-sharing): finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {DeviceInformationComponent} from "./components/device-information/device-information.component";
+import {LocationSharingComponent} from "./components/location-sharing/location-sharing.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DeviceInformationComponent],
+  imports: [RouterOutlet, DeviceInformationComponent, LocationSharingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
