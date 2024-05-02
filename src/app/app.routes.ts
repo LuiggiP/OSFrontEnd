c9de77653b767a3a2d19fb509b376ec3926d38be
Fabrizio0711 <u202213652@upc.edu.pe>
2024-05-01 23:39:04 -0500

    feat: added routes to join component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import {VehicleInformationComponent} from "./components/vehicle-information/vehicle-information.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {LoginComponent} from "./components/login/login.component";
+import {MainScreenComponent} from "./components/main-screen/main-screen.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {DeviceInformationComponent} from "./components/device-information/device-information.component";
+import {LocationSharingComponent} from "./components/location-sharing/location-sharing.component";
+import {VehicleTrackingComponent} from "./components/vehicle-tracking/vehicle-tracking.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'main-screen', component: MainScreenComponent
+  },
+  {
+    path: 'profile', component: ProfileComponent
+  },
+  {
+    path: 'device-information', component: DeviceInformationComponent
+  },
+  {
+    path: 'location-sharing', component: LocationSharingComponent
+  },
+  {
+    path: 'vehicle-information', component: VehicleInformationComponent
+  },
+  {
+    path: 'vehicle-tracking', component: VehicleTrackingComponent
+  }
+
+];
