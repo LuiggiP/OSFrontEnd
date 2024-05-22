170f3d5ca4425dc942f1133cfb2ff2cd337b6fe0
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-05-22 15:34:44 -0500

    feat: refactored components names

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {DeviceInformationComponent} from "./domain/device/device-information/device-information.component";
-import {LoginSectionComponent} from "./domain/components/login-section/login-section.component"
+import {LoginSectionComponent} from "./domain/user/login/login-section/login-section.component"
 import {CommonRegisterSectionComponent} from "./domain/user/register/common-register-section/common-register-section.component";
 import {TaxiDriverRegisterComponent} from "./domain/user/register/taxiDriver-register-section/taxiDriver-register-section";
 import {VehicleInformationComponent} from "./domain/vehicle/vehicle/vehicle-information/vehicle-information.component";
 import {VehicleTrackingComponent} from "./domain/vehicle/tracking/vehicle-tracking/vehicle-tracking.component";
-import {ProfileSectionComponent} from "./public/components/profile-section/profile-section.component";
+import {ProfileSectionComponent} from "./domain/user/profile/profile-section/profile-section.component";
 import {TheStartComponent} from "./shared/start/start-page/the-start.component";
 import {DebugToolbarComponent} from "./test/components/debug-toolbar/debug-toolbar.component";
 
--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import {VehicleInformationComponent} from "../core/domain/vehicle/vehicle/vehicle-information/vehicle-information.component";
 import {CommonRegisterSectionComponent} from "../core/domain/user/register/common-register-section/common-register-section.component";
-import {LoginSectionComponent} from "../core/domain/components/login-section/login-section.component";
+import {LoginSectionComponent} from "../core/domain/user/login/login-section/login-section.component";
 import {TheStartComponent} from "../core/shared/start/start-page/the-start.component";
-import {ProfileSectionComponent} from "../core/public/components/profile-section/profile-section.component";
+import {ProfileSectionComponent} from "../core/domain/user/profile/profile-section/profile-section.component";
 import {DeviceInformationComponent} from "../core/domain/device/device-information/device-information.component";
 import {VehicleTrackingComponent} from "../core/domain/vehicle/tracking/vehicle-tracking/vehicle-tracking.component";
 import {TaxiDriverRegisterComponent} from "../core/domain/user/register/taxiDriver-register-section/taxiDriver-register-section";
