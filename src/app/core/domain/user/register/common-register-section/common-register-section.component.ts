f12fc711d17e3fc5cd7d3df8df09d692320b9f00
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-05-22 15:58:41 -0500

    feat: added information to register

--- src/app/core/domain/user/register/common-register-section/common-register-section.component.ts
+++ src/app/core/domain/user/register/common-register-section/common-register-section.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {MatCard, MatCardContent} from "@angular/material/card";
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormFieldModule, MatHint} from '@angular/material/form-field';
 import {MatInput} from "@angular/material/input";
 import {Router} from "@angular/router";
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import {provideNativeDateAdapter} from "@angular/material/core";
 
 @Component({
   selector: 'app-common-register-section',
@@ -16,8 +18,13 @@ import {Router} from "@angular/router";
     MatCardContent,
     MatFormFieldModule,
     MatInput,
-    MatButton
+    MatButton,
+    MatDatepicker,
+    MatDatepickerToggle,
+    MatDatepickerInput,
+    MatHint
   ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './common-register-section.component.html',
   styleUrl: './common-register-section.component.css'
 })
--- src/app/core/domain/user/register/taxiDriver-register-section/taxiDriver-register-section.ts
+++ src/app/core/domain/user/register/taxiDriver-register-section/taxiDriver-register-section.ts
@@ -7,22 +7,28 @@ import {MatInput} from "@angular/material/input";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
 import {Router} from "@angular/router";
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import {provideNativeDateAdapter} from "@angular/material/core";
 
 
 @Component({
   selector: 'app-common-register-section',
   standalone: true,
-  imports: [
-    MatIconButton,
-    MatIcon,
-    MatCard,
-    MatCardContent,
-    MatFormFieldModule,
-    MatInput,
-    MatOption,
-    MatSelect,
-    MatButton
-  ],
+    imports: [
+        MatIconButton,
+        MatIcon,
+        MatCard,
+        MatCardContent,
+        MatFormFieldModule,
+        MatInput,
+        MatOption,
+        MatSelect,
+        MatButton,
+        MatDatepicker,
+        MatDatepickerInput,
+        MatDatepickerToggle
+    ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './taxiDriver-register-section.html',
   styleUrl: './taxiDriver-register-section.css'
 })
