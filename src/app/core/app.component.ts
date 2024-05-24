c1d99774cd22d7c0f611d860a85d1aeee267f1fc
LuiggiP <u202218996@upc.edu.pe>
2024-05-24 17:09:53 -0500

    refactor(register-section): removed the unnecessary imports

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -3,7 +3,6 @@ import {RouterLink, RouterOutlet} from '@angular/router';
 import {DeviceInformationComponent} from "./domain/device/device-information/device-information.component";
 import {LoginSectionComponent} from "./domain/user/login/login-section/login-section.component"
 import {CommonRegisterSectionComponent} from "./domain/user/register/common-register-section/common-register-section.component";
-import {TaxiDriverRegisterComponent} from "./domain/user/register/taxiDriver-register-section/taxiDriver-register-section";
 import {VehicleInformationComponent} from "./domain/vehicle/vehicle/vehicle-information/vehicle-information.component";
 import {VehicleTrackingComponent} from "./domain/vehicle/tracking/vehicle-tracking/vehicle-tracking.component";
 import {ProfileSectionComponent} from "./domain/user/profile/profile-section/profile-section.component";
@@ -13,7 +12,7 @@ import {DebugToolbarComponent} from "./test/components/debug-toolbar/debug-toolb
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, DeviceInformationComponent, LoginSectionComponent, CommonRegisterSectionComponent, TaxiDriverRegisterComponent, VehicleInformationComponent, VehicleTrackingComponent, ProfileSectionComponent, TheStartComponent, DebugToolbarComponent],
+  imports: [RouterOutlet, RouterLink, DeviceInformationComponent, LoginSectionComponent, CommonRegisterSectionComponent, VehicleInformationComponent, VehicleTrackingComponent, ProfileSectionComponent, TheStartComponent, DebugToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
