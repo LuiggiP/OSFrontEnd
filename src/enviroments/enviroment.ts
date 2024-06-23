d1bbd36363c7fe19d4c3a2db49cbc61614a96da3
LuiggiP <u202218996@upc.edu.pe>
2024-06-23 14:57:15 -0500

    feat(environments): added new env for azure deploy

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 ﻿// src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://ec2-54-226-90-201.compute-1.amazonaws.com:8080/api/v1'
+  apiUrl: 'https://securcaros.azurewebsites.net:80/api/v1'
 };
